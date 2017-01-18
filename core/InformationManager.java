e71c3b7e23482710298276380635b71453e4b0c0
Ghabs95 <ghabs95@gmail.com>
2017-01-18 18:38:51 +0100

    Modifica path immagine nella classe Preface

--- core/InformationManager.java
+++ core/InformationManager.java
@@ -15,47 +15,46 @@ public class InformationManager {
 	public final static int OGGI = ForecastTree.OGGI;
 	public final static int DOMANI = ForecastTree.DOMANI;
 	public final static int DOPODOMANI = ForecastTree.DOPODOMANI;
-	
+
 	private ForecastTree ft;
-	
-	public InformationManager(){
-		ft = new ForecastTree();				
+
+	public InformationManager() {
+		ft = new ForecastTree();
 	}
-	
-	
-	public void gatherForecastLocation(int meteoID, String location){
+
+	public void gatherForecastLocation(int meteoID, String location) {
 		ft.createLocationForecast(meteoID, location);
 	}
-	
-	public String getPrintableForecast(int meteoID, String location, int dayID){
+
+	public String getPrintableForecast(int meteoID, String location, int dayID) {
 		Forecast tmp = ft.getDayForecast(meteoID, location, dayID);
-		String str = tmp.toString();	
+		String str = tmp.toString();
 		return str;
 	}
-	
-	public BufferedImage getMeteoSatImage(){
+
+	public BufferedImage getMeteoSatImage() {
 		String path = "http://img2.meteo.it/forecastimg/realtime/europa/eu_sat_07.jpg";
 		return WebHandler.getInstance().getPhoto(path);
 	}
-	
-	//Solo per Esempio!
-	public void showAll(){
-		Map<String,ArrayList<Forecast>> meteoForecast;
+
+	// Solo per Esempio!
+	public void showAll() {
+		Map<String, ArrayList<Forecast>> meteoForecast;
 		System.out.println("\n###### LAMMA #####");
 		meteoForecast = ft.getForecastByMeteo(LAMMA);
 		meteoForecast.forEach((loc, fullForecast) -> printMap(loc, fullForecast));
-		
+
 		System.out.println("\n###### METEO AM #####");
 		meteoForecast = ft.getForecastByMeteo(AM);
 		meteoForecast.forEach((loc, fullForecast) -> printMap(loc, fullForecast));
-		
+
 		System.out.println("\n###### 3B Meteo #####");
 		meteoForecast = ft.getForecastByMeteo(THREEB);
 		meteoForecast.forEach((loc, fullForecast) -> printMap(loc, fullForecast));
 	}
-	
+
 	private void printMap(String location, ArrayList<Forecast> fullForecast) {
-		System.out.println("\n+++++"+location.toUpperCase()+"+++++");
+		System.out.println("\n+++++" + location.toUpperCase() + "+++++");
 		fullForecast.forEach(forecast -> forecast.show());
 		System.out.println("\n");
 	}
--- core/forecast/ForecastTree.java
+++ core/forecast/ForecastTree.java
@@ -17,71 +17,78 @@ public class ForecastTree {
 	public final static int OGGI = ForecastAbstractFactory.OGGI;
 	public final static int DOMANI = ForecastAbstractFactory.DOMANI;
 	public final static int DOPODOMANI = ForecastAbstractFactory.DOPODOMANI;
-	
-	private ArrayList<Map<String,ArrayList<Forecast>>> forecastTree;
+
+	private ArrayList<Map<String, ArrayList<Forecast>>> forecastTree;
 	private Lamma lamma;
 	private MeteoAM am;
 	private ThreeB threeB;
-	private int[] dayIDs = {OGGI,DOMANI,DOPODOMANI};
-	private int[] meteoIDs = {LAMMA,AM,THREEB};
-	
-	
-	public ForecastTree(){
+	private int[] dayIDs = { OGGI, DOMANI, DOPODOMANI };
+	private int[] meteoIDs = { LAMMA, AM, THREEB };
+
+	public ForecastTree() {
 		lamma = new Lamma("http://www.lamma.rete.toscana.it/previ/ita/xml/comuni_web/dati/");
 		am = new MeteoAM("http://www.meteoam.it/ta/previsione/");
 		threeB = new ThreeB("http://www.3bmeteo.com/meteo/");
-		//inizializzo l'albero
+		// inizializzo l'albero
 		forecastTree = new ArrayList<>();
-		for(int i=0;i<3;i++){
+		for (int i = 0; i < 3; i++) {
 			forecastTree.add(new HashMap<>());
 		}
 	}
-	
-	public void createLocationForecast(int meteoID, String location){
-		ArrayList<Forecast> fullForecast;		
-		switch(meteoID){
-		case(LAMMA): {fullForecast = getFullForecast(lamma , location); break;}
-		case(AM):    {fullForecast = getFullForecast(am    , location); break;}
-		case(THREEB):{fullForecast = getFullForecast(threeB, location); break;}
-		default: {fullForecast = new ArrayList<>();}
+
+	public void createLocationForecast(int meteoID, String location) {
+		ArrayList<Forecast> fullForecast;
+		switch (meteoID) {
+		case (LAMMA): {
+			fullForecast = getFullForecast(lamma, location);
+			break;
 		}
-		forecastTree.get(meteoID).put(location, fullForecast);	
+		case (AM): {
+			fullForecast = getFullForecast(am, location);
+			break;
+		}
+		case (THREEB): {
+			fullForecast = getFullForecast(threeB, location);
+			break;
+		}
+		default: {
+			fullForecast = new ArrayList<>();
+		}
+		}
+		forecastTree.get(meteoID).put(location, fullForecast);
 	}
-	
-	private ArrayList<Forecast> getFullForecast(Site meteo, String location){
+
+	private ArrayList<Forecast> getFullForecast(Site meteo, String location) {
 		ArrayList<Forecast> tmp = new ArrayList<>();
-		for(int day:dayIDs){
+		for (int day : dayIDs) {
 			tmp.add(meteo.getForecast(location, day));
 		}
 		return tmp;
 	}
-	
-	
+
 	/* Metodi per prendere le informazioni dall'albero */
-	
-	public Map<String,ArrayList<Forecast>> getForecastByMeteo(int meteoID){
+	public Map<String, ArrayList<Forecast>> getForecastByMeteo(int meteoID) {
 		return forecastTree.get(meteoID);
 	}
-	
-	public ArrayList<ArrayList<Forecast>> getForecastByLocation(String location){
+
+	public ArrayList<ArrayList<Forecast>> getForecastByLocation(String location) {
 		ArrayList<ArrayList<Forecast>> byLoc = new ArrayList<>();
-		for(int meteo:meteoIDs){
+		for (int meteo : meteoIDs) {
 			byLoc.add(forecastTree.get(meteo).get(location));
 		}
 		return byLoc;
 	}
-	
-	public ArrayList<Forecast> getForecastByDay(String location, int dayID){
+
+	public ArrayList<Forecast> getForecastByDay(String location, int dayID) {
 		ArrayList<Forecast> byDay = new ArrayList<>();
-		for(int meteo:meteoIDs){
-			byDay.add(getDayForecast(meteo,location,dayID));
+		for (int meteo : meteoIDs) {
+			byDay.add(getDayForecast(meteo, location, dayID));
 		}
 		return byDay;
 	}
-	
-	public Forecast getDayForecast(int meteoID, String location, int dayID){
+
+	public Forecast getDayForecast(int meteoID, String location, int dayID) {
 		return forecastTree.get(meteoID).get(location).get(dayID);
 	}
-	
-	
+
 }
