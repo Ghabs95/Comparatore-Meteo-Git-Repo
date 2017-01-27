8c0aeb735d83995d31aff4b70b1ef1c04da9e292
Lorenzo <lore.sarti.93@gmail.com>
2017-01-27 16:05:12 +0100

    Creati pacchetti Schemi

--- gui/search/childs/Preface.java
+++ gui/search/childs/Preface.java
@@ -42,7 +42,7 @@ public class Preface extends GridBagContainer {
 	}
 	
 	private BufferedImage getMeteoSatImage(){
-		String path = "http://img2.meteo.it/forecastimg/realtime/europa/eu_sat_07.jpg"; //TODO #check: cercare un'altra immagine con url stabile
+		String path = "http://img2.meteo.it/forecastimg/realtime/europa/eu_sat_07.jpg";
 		BufferedImage image = WebHandler.getInstance().getPhoto(path); 
 		return resize(image,250,150);
 	}
